eb0bef1da23735dd6fefd89dab724d5b400b3118
fabila-collab <luxusoden@gmail.com>
2026-07-01 22:03:38 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,20 @@
-
+CREATE DATABASE IF NOT EXITS veridoc;
+USE veridoc;
+CREATE TABLE etablissements (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nom VARCHAR(150) NOT NULL,
+  email VARCHAR(150) UNIQUE NOT NULL,
+  mot_de_passe VARCHAR(255) NOT NULL,
+  date_creation DATETIME DEFAULT CURRENT_TIMESTAM
+  );
+CREATE TABLE documents (
+  id INT AUTO_INCREMENTS PRIMARY KEY,
+  code_unique VARCHAR(50) UNIQUE NOT NULL,
+  nom_titulaire VARCHAR(150) NOT NULL,
+  type_document VARCHAR(100) NOT NULL,
+  etablissement_id INT NOT NULL,
+  date_emission DATE NOT NULL,
+  statut ENUM('valide' , 'revoque') DEFAULT 'valide',
+  date_creation DATETIME DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (etablissement_id) REFERENCES etablissements(id)
+  );
